ffd2fa14cc05df6880e5fb3e6348a563aec458d2
1990frog <1990frog@gmail.com>
2021-06-03 17:28:12 +0800

    commit

--- config.h
+++ config.h
@@ -89,15 +89,10 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	// dmenu
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	// terimal
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-        // 隐藏bar  
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	// 顺时针切换
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	// 逆时针切换
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
