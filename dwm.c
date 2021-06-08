e199b227a9bec30a63af7030ac5d94dcf0365afb
1990frog <1990frog@gmail.com>
2021-06-08 15:47:08 +0000

    commit

--- dwm.c
+++ dwm.c
@@ -1935,8 +1935,8 @@ sigdwmblocks(const Arg *arg)
 void
 spawn(const Arg *arg)
 {
-	if (arg->v == dmenucmd)
-		dmenumon[0] = '0' + selmon->num;
+	//if (arg->v == dmenucmd)
+	dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
